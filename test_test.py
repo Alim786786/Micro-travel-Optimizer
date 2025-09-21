4a099937715887466720a3a8cfd193d07a0d15fd
Alim786786 <alim.pathan@mail.utoronto.ca>
2025-09-21 00:34:46 -0500

    Update test_test.py

--- test_test.py
+++ test_test.py
@@ -11,6 +11,7 @@ output = replicate.run(
     input={"prompt": "Write a short motivational quote.", "max_tokens": 50}
 )
 print("poop")
+print("no")
 # Join chunks into one string
 result = "".join(output).strip()
 print("AI says:", result)
